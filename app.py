f827aa305add3789c9bbbd551d4e1885aa5197b8
yogesh1983kan-maker <yogesh1983kan@gmail.com>
2026-02-28 00:51:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import re
 import numpy as np
 import traceback
 
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 app = Flask(__name__)
 
 FILE_PATH = "Data.xlsx"
